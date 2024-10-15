f470c001c40c14d4abfb606da5a5b2442cf53194
cyrus01337 <cyrus21337@gmail.com>
2024-10-15 16:27:51 +0000

    Add alias for pulling images

--- conf.d/aliases/docker.fish
+++ conf.d/aliases/docker.fish
@@ -6,5 +6,6 @@ if which docker &> /dev/null
     alias dcb="dc build"
     alias dcr="dc down && dc up"
     alias di="docker image ls"
+    alias dp="docker pull"
     alias docker-images="docker image ls"
 end
