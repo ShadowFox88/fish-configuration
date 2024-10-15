7443671270fceaea6c0e44c5a2b485da7e672411
cyrus01337 <cyrus21337@gmail.com>
2024-10-15 18:21:20 +0000

    Set default editor to Vi

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
-set -x EDITOR "nvim"
+set -x EDITOR "vi"
 set -x GNUPGHOME "$HOME/.local/gnupg"
 set -x INPUTRC "$HOME/.inputrc"
 set -x LANG "en_GB.UTF-8"
 set -x LANGUAGE "en_GB:en"
 set -x PASSWORD_STORE_DIR "$HOME/.local/password-store"
-set -x VISUAL "nvim"
+set -x VISUAL "vi"
 set -x XDG_CACHE_HOME "$HOME/.cache"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -Ux XDG_DATA_DIRS "/var/lib/flatpak/exports/share" "$HOME/.local/share/flatpak/exports/share" "/usr/share/ubuntu" "/usr/share/gnome" "/usr/local/share/" "/usr/share/"
