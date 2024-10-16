e686bfc40f0df649dbaf5055dd0f961730097f37
cyrus01337 <cyrus21337@gmail.com>
2024-10-16 16:32:48 +0000

    Recreate containers

--- conf.d/aliases/devpod.fish
+++ conf.d/aliases/devpod.fish
@@ -3,4 +3,9 @@ if which devpod &> /dev/null
     function devpod-ssh-cwd --wraps ssh
         ssh (basename $PWD).devpod
     end
+
+    function devpod-recreate --wraps "devpod up"
+        devpod down . &> /dev/null
+        devpod up --recreate .
+    end
 end
