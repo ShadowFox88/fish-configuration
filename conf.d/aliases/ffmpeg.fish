0de5bc73528f7deb6906a75f78e2ecff13468292
cyrus01337 <cyrus21337@gmail.com>
2024-10-16 16:18:05 +0000

    Simplify parameters usage

--- conf.d/aliases/ffmpeg.fish
+++ conf.d/aliases/ffmpeg.fish
@@ -1,15 +1,21 @@
 #!/usr/bin/env fish
 if which ffmpeg &> /dev/null
     function ffmpeg-concat --wraps ffmpeg
-        ffmpeg -f concat -safe 0 -i "$argv[1]" -c copy "$argv[2]"
+        set input $argv[1]
+        set output $argv[2]
+
+        ffmpeg -f concat -safe 0 -i $input -c copy $output
     end
 
     function ffmpeg-to-mp4 --wraps ffmpeg
-        ffmpeg -i "$argv[1]" -codec copy "$argv[2]"
+        set input $argv[1]
+        set output $argv[2]
+
+        ffmpeg -i $input -codec copy $output
     end
 
     function ffmpeg-generate-inputs
-        set count "$argv[1]"
+        set count $argv[1]
 
         for number in (seq 1 $count)
             echo "file '$PWD/$number.mkv'" >> inputs.txt
