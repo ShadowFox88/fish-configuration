11b512fe0f4db9c0282c81e2a08aee025a637011
cyrus01337 <cyrus21337@gmail.com>
2024-10-16 16:00:44 +0000

    Allow for non-before scripts to be reloaded

--- conf.d/init.fish
+++ conf.d/init.fish
@@ -13,45 +13,32 @@ if ! set -q RAN_BEFORE_SCRIPTS
     set RAN_BEFORE_SCRIPTS true
 end
 
-if ! set -q INITIALISED_MODULES
-    for module in $__fish_config_dir/conf.d/modules/*.fish
-        source $module
+for module in $__fish_config_dir/conf.d/modules/*.fish
+    source $module
 
-        set cached_status $status
+    set cached_status $status
 
-        if [ $cached_status != 0 ]
-            echo "Module $module failed with exit code $cached_status"
-        end
+    if [ $cached_status != 0 ]
+        echo "Module $module failed with exit code $cached_status"
     end
-
-    set INITIALISED_MODULES false
 end
 
-if ! set -q INITIALISED_PACKAGES
-    for package in $__fish_config_dir/conf.d/packages/*.fish
-        source $package
+for package in $__fish_config_dir/conf.d/packages/*.fish
+    source $package
 
-        set cached_status $status
+    set cached_status $status
 
-        if [ $cached_status != 0 ]
-            echo "Package $package failed with exit code $cached_status"
-        end
+    if [ $cached_status != 0 ]
+        echo "Package $package failed with exit code $cached_status"
     end
-
-    set INITIALISED_PACKAGES false
 end
 
-if ! set -q INITIALISED_ALIASES
-    for aliases in $__fish_config_dir/conf.d/aliases/*.fish
-        source $aliases
+for aliases in $__fish_config_dir/conf.d/aliases/*.fish
+    source $aliases
 
-        set cached_status $status
+    set cached_status $status
 
-        if [ $cached_status != 0 ]
-            echo "Aliases script $aliases failed with exit code $cached_status"
-        end
+    if [ $cached_status != 0 ]
+        echo "Aliases script $aliases failed with exit code $cached_status"
     end
-
-    set INITIALISED_ALIASES false
 end
-
