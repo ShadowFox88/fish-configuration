f2399ccfbba744265aff046895c6dcb8646e26c7
cyrus01337 <cyrus21337@gmail.com>
2024-10-18 07:28:32 +0000

    Add command to copy straight to clipboard in readable format

--- conf.d/aliases/clip.fish
+++ conf.d/aliases/clip.fish
@@ -4,3 +4,11 @@ if [ "$XDG_SESSION_TYPE" = "x11" ]; and which xclip &> /dev/null
 else if [ "$XDG_SESSION_TYPE" = "wayland" ]; and which wl-copy &> /dev/null
     alias clip="wl-copy"
 end
+
+if which clip &> /dev/null
+    function copy-to-clipboard
+        set target $argv[1]
+
+        cat $target | clip
+    end
+end
