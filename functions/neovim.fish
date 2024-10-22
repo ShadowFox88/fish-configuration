3b88370ca0d92e66919e46a83c8693719be0c715
cyrus01337 <cyrus21337@gmail.com>
2024-10-22 09:10:35 +0000

    Tidy + ensure target is processed correctly

--- functions/neovim.fish
+++ functions/neovim.fish
@@ -12,17 +12,24 @@ if which nvim &> /dev/null
 else if which docker &> /dev/null
     function n --wraps "zsh"
         set target $argv[1]
-        set default_flags -it --rm --name (basename (realpath $target))
+        set default_flags -it --rm
         set default_volumes -v $HOME/.gitconfig:/home/developer/.gitconfig -v $HOME/.git-credentials:/home/developer/.git-credentials -v neovim-data:/home/developer/.local/share/nvim
+        set arguments $argv[2..]
 
         if not [ $target ]
-            docker run $default_flags $default_volumes -v $HOME/.config/nvim:/workspace cyrus01337/neovim-devcontainer:latest -c "nvim . $argv[2..]"
-        else if [ -d $target ]
-            docker run $default_flags $default_volumes -v (realpath $target):/workspace cyrus01337/neovim-devcontainer:latest $argv[2..]
-        else
-            set filename (basename (realpath $target))
+            docker run $default_flags --name neovim-configuration $default_volumes -v $HOME/.config/nvim:/workspace cyrus01337/neovim-devcontainer:latest -c "nvim . $arguments"
 
-            docker run $default_flags $default_volumes -v (realpath $target):/workspace/$filename cyrus01337/neovim-devcontainer:latest -c "nvim $filename" $argv[2..]
+            return 0
+        end
+
+        set absolute_path_to_target (realpath $target)
+        set filename (basename $absolute_path_to_target)
+        set default_flags $default_flags --name $filename
+
+        if [ -d $target ]
+            docker run $default_flags $default_volumes -v $absolute_path_to_target:/workspace cyrus01337/neovim-devcontainer:latest $arguments
+        else
+            docker run $default_flags $default_volumes -v $absolute_path_to_target:/workspace/$filename cyrus01337/neovim-devcontainer:latest -c "nvim $filename" $arguments
         end
     end
 end
